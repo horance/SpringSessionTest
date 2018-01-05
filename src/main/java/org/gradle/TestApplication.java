1d72c1bbc68ee53b9cbd8bd0d1920d3cd92e86a3
Horance Chou <horance@gmail.com>
2018-01-05 17:52:47 +0800

    remove unused test code

--- src/main/java/org/gradle/TestApplication.java
+++ src/main/java/org/gradle/TestApplication.java
@@ -1,18 +1,11 @@
 package org.gradle;
 
-import java.util.Arrays;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.http.MediaType;
-import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,19 +29,4 @@ public class TestApplication {
 		return "Hello, " + (StringUtils.isEmpty(name) ? " there " : name) + ". " + session.getId();
 	}
 	
-	@Bean
-	public RedisMessageListenerContainer redisMessageListenerContainer(
-			RedisConnectionFactory connectionFactory,
-			RedisOperationsSessionRepository messageListener) {
-		System.out.println("running in customized config");
-		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.addMessageListener(messageListener,
-				Arrays.asList(new PatternTopic("__keyevent@*:del"),
-						new PatternTopic("__keyevent@*:expired")));
-		container.addMessageListener(messageListener, Arrays.asList(new PatternTopic(
-				messageListener.getSessionCreatedChannelPrefix() + "*")));
-		return container;
-	}
-
 }
