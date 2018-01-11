82d0908426323f8f85938049239179be2474ee02
Horance Chou <horance@gmail.com>
2018-01-11 16:52:17 +0800

    add session listener

---
+++ src/main/java/org/gradle/MySessionListener.java
@@ -0,0 +1,20 @@
+package org.gradle;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class MySessionListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+
+        System.out.println("Session created: " + se.getSession().getId());
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+
+        System.out.println("Session destroyed: " + se.getSession().getId());
+    }
+
+}
